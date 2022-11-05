e3950172f8b27c46e6d14b69d60a509dd418fd67
oscarvx00 <oscarvicente@usal.es>
2022-11-05 20:21:24 +0100

    Fixed gitguardian leak

--- src/youtube_manager/youtube_manager.py
+++ src/youtube_manager/youtube_manager.py
@@ -8,7 +8,7 @@ YOUTUBE_API_KEY = os.environ['YOUTUBE_API_KEY']
 
 def search_song(song_name : str):
 
-    url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCXSyAhli8w0E1o5dGeUylVtEkiSpdedrI&part=id&q=" + urllib.parse.quote(song_name)
+    url = f'https://www.googleapis.com/youtube/v3/search?key={YOUTUBE_API_KEY}&part=id&q={urllib.parse.quote(song_name)}'
 
     response = requests.get(url=url)
     response_data = response.json()
